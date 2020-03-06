1d58533024d6aa6c7c76aa392450d219aa8395be
manasaganesh <manasa.ncm7@gmail.com>
2020-03-06 07:44:02 -0500

    Linked list {WIP}

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -10,27 +10,61 @@ public class LinkedList {
             head = node;
             return;
         }
-        Node cur = head;
-        while(cur.getNext()!=null)
+        if(head.next==null)
         {
-            cur = cur.getNext();
+            head.next= node;
+            return;
         }
-        cur.setNext(node);
+        Node cur = head;
+        while(cur.next!=null)
+        {
+            cur = cur.next;
+        }
+        cur.next = node;
 
     }
 
     public void displayNodes() {
         if(head == null)
             return;
-        if(head.getNext() == null)
+        if(head.next == null)
         {
-            System.out.println(head.getValue());
+            System.out.println(head.value);
+            return;
         }
         Node cur = head;
-        while(cur.getNext()!=null)
+        while(cur!=null)
         {
-            System.out.println(cur.getValue());
-            cur = cur.getNext();
+            System.out.println(cur.value);
+            cur = cur.next;
         }
+    }
+
+    /**
+     * This method can be implemented by  traversing linked list and adding data to set , If adding to set returns null remove the pointed and adjust the list
+     * 2nd approach is o use 2 loops through runner technique implemented below
+     */
+    public void removeDuplicates() {
+        if(head == null || head.next == null)
+            return;
+        while(head.next!=null)
+        {
+            Node runner = head.next;
+            Node prev = head;
+            while(runner.next!=null){
+                if(head.value == runner.value)
+                {
+                    runner.next = runner.next.next;
+                }
+                else
+                {
+                    prev = runner;
+                    runner = runner.next;
+
+                }
+            }
+            head = head.next;
+        }
+        
     }
 }
--- src/linkedlist/RemoveDupes.java
+++ src/linkedlist/RemoveDupes.java
@@ -6,7 +6,21 @@ public class RemoveDupes {
         LinkedList linkedList = new LinkedList();
         linkedList.add( 1);
         linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add( 1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add( 1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add( 1);
+        linkedList.add(2);
+        linkedList.add(3);
 
+
+        linkedList.displayNodes();
+        linkedList.removeDuplicates();
+        System.out.println("Linked list after removing duplicates");
         linkedList.displayNodes();
     }
 
