a04233e05a493db18c72600ad47f6facc6b87bba
Manasa <manasa.ncm7@gmail.com>
2020-04-17 08:14:49 -0400

    BST :Calculate branch SUms

--- src/trees/BstConstruction.java
+++ src/trees/BstConstruction.java
@@ -1,5 +1,8 @@
 package trees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author mchitrashekar
  */
@@ -102,6 +105,26 @@ public class BstConstruction {
         }
     }
 
+    public static List<Integer> branchSums(BST root) {
+        List<Integer> brancSumsList = new ArrayList<>();
+        calculateBranchSum(root,0,brancSumsList);
+        return brancSumsList;
+    }
+
+    private static void calculateBranchSum(BST root, int sum, List<Integer> list) {
+        if(root == null)
+            return;
+        sum = sum + root.value;
+        if(root.left== null && root.right == null)
+        {
+
+            list.add(sum);
+            return;
+        }
+        calculateBranchSum(root.left,sum,list);
+        calculateBranchSum(root.right,sum,list);
+    }
+
 
     static BST root = new BST(10);
     BST test3 = new BST(10);
@@ -112,29 +135,32 @@ public class BstConstruction {
                 .insert(5)
                 .insert(15)
                 .insert(5)
-                .insert(2)
-                .insert(1)
-                .insert(22)
-                .insert(1)
-                .insert(1)
-                .insert(3)
-                .insert(1)
-                .insert(1)
-                .insert(502)
-                .insert(55000)
-                .insert(204)
-                .insert(205)
-                .insert(207)
-                .insert(206)
-                .insert(208)
-                .insert(203)
-                .insert(-51)
-                .insert(-403)
-                .insert(1001)
-                .insert(57)
-                .insert(60)
-                .insert(4500);
-        test.inorder();
-        System.out.println(test.findClosestValueInBst(test,-70));
+                .insert(2);
+//                .insert(1)
+//                .insert(22)
+//                .insert(1)
+//                .insert(1)
+//                .insert(3)
+//                .insert(1)
+//                .insert(1)
+//                .insert(502)
+//                .insert(55000)
+//                .insert(204)
+//                .insert(205)
+//                .insert(207)
+//                .insert(206)
+//                .insert(208)
+//                .insert(203)
+//                .insert(-51)
+//                .insert(-403)
+//                .insert(1001)
+//                .insert(57)
+//                .insert(60)
+//                .insert(4500);
+//        test.inorder();
+        List<Integer> resultList = branchSums(test);
+        for (Integer s:resultList) {
+            System.out.println(s);
+        }
     }
 }
