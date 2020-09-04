a854761c375a493b6e4d92927582f2aa5bfc6614
manasaganesh <manasa.ncm7@gmail.com>
2020-09-03 22:57:43 -0400

    Node depth for bst

--- src/arraysandstring/CipherEncryptor.java
+++ src/arraysandstring/CipherEncryptor.java
@@ -11,20 +11,12 @@ public class CipherEncryptor {
     public static String caesarCypherEncryptor(String str, int key) {
       if(key==0)
           return str;
-        Map<Character,Character> lookUpTable = new HashMap();
-        char[] valueArray = new char[26];
-        for(int i = 0;i< valueArray.length;i++)
-        {
-            valueArray[i] = (char) (97+i);
-        }
         StringBuilder sb = new StringBuilder();
         for(char c : str.toCharArray())
         {
-           int shiftChar = (c-97)+ key;
-           if(shiftChar<= 25)
-            sb.append(valueArray[shiftChar]);
-           else
-               sb.append(valueArray[key % 25]);
+            char ch = (char)(((int)c+
+                    key - 65) % 26 + 65);
+            sb.append(ch);
         }
         return sb.toString();
     }
